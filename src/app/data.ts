d5a0cd41c349a64ebb22fd9a76b37d3c0e9016ec
mateuszwicherkski <wicherskimateusz@gmail.com>
2024-08-26 16:53:29 +0200

    DATA FOR ONE CASE

---
+++ src/app/data.ts
@@ -0,0 +1,76 @@
+export const DATA = {
+    cases: [
+            {
+            id: '1',
+            title: 'Treachery in build castle',
+            suspects: [
+                {
+                    id: '1',
+                    name: 'Cambrige Jenkins',
+                    image: '../../assets/256.png',
+                    description: 'Likes to build stuff but for some unknown reasons throws big stack trace.'
+                },
+                {
+                    id: '2',
+                    name: 'Heavy Lifter Gopher',
+                    image: '../assets/gopher_lifter.png',
+                    description: 'He runs everything in this place no job or constainer will do enything without him.'
+                },
+                {
+                    id: '3',
+                    name: 'Fluffy',
+                    image: '../assets/go-fuzz.png',
+                    description: 'He like to play all kind of medival games. That is how he got his shield.'
+                }
+            ],
+            places: [
+                {
+                    id: '1',
+                    name: 'EC2',
+                    image: '../assets/ec2.png',
+                    description: 'Nobody knows how this place spawns and vanishes.'
+                },
+                {
+                    id: '2',
+                    image: '../assets/arti.png',
+                    name: 'Artifactory',
+                    description: 'A factory for artifacts but not for building.'
+                },
+                {
+                    id: '3',
+                    image: '../assets/m2.png',
+                    name: '.m2',
+                    description: 'Nobody know what can you find inside it.'
+                }
+            ],
+            weapons: [
+                {
+                    id: '1',
+                    name: 'InteliJ',
+                    image: '../assets/ij.png',
+                    description: 'Heavy IDE with lots of features.'
+                },
+                {
+                    id: '2',
+                    name: 'OH MY ZSH',
+                    image: '../assets/zsh.png',
+                    description: 'There is no color that won\'t came with it.'
+                },
+                {
+                    id: '3',
+                    name: 'Memory',
+                    image: '../assets/mem.png',
+                    description: 'Might cause objects to dissapear if too many.'
+                },
+            ],
+            clues: [
+                "clue1",
+                "clue2",
+                "clue3",
+                "clue4",
+                "clue5"
+            ]
+        
+        }
+    ]
+}
