1ff9f397f61cb93d251285d2548531aade2b0ceb
mateuszwicherkski <wicherskimateusz@gmail.com>
2024-08-27 18:14:40 +0200

    Accusation button with bad dialog

---
+++ src/app/accusation-dialog/accusation-dialog.component.ts
@@ -0,0 +1,33 @@
+import { NgFor } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccusationModel } from './acusationForm.model';
+
+@Component({
+  selector: 'app-accusation-dialog',
+  standalone: true,
+  imports: [FormsModule, NgFor],
+  templateUrl: './accusation-dialog.component.html',
+  styleUrl: './accusation-dialog.component.css'
+})
+export class AccusationDialogComponent {
+
+  @Output() close = new EventEmitter();
+  selectedWhere: string = '';
+  selectedWho: string = '';
+  selectedHow: string = '';
+
+  whereOptions = ['Here', 'There', 'Anywhere'];
+  howOptions = ['Option1', 'Option2', 'Option3'];
+  whoOptions = ['Option1', 'Option2', 'Option3'];
+
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    // TODO: Some walidation and maybe toast if correct or wrong? Do it by service
+    console.log("Are you sure?");
+    this.close.emit();
+  }
+}
---
+++ src/app/accusation-dialog/acusationForm.model.ts
@@ -0,0 +1,5 @@
+export interface AccusationModel {
+    who: string,
+    how: string,
+    where: string,
+}
