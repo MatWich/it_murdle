016555491daa34812d5f62643f15dc50f9fef150
mateuszwicherkski <wicherskimateusz@gmail.com>
2024-09-08 15:41:26 +0200

    deduction grid with suspects images

--- src/app/deduction-map/deduction-grid/deduction-grid.component.ts
+++ src/app/deduction-map/deduction-grid/deduction-grid.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MurdleService } from '../../murdle.service';
 import { DeductionSuspectComponent } from '../deduction-suspect/deduction-suspect.component';
 import { DeductionTileComponent } from '../deduction-tile/deduction-tile.component';
 
@@ -10,5 +11,50 @@ import { DeductionTileComponent } from '../deduction-tile/deduction-tile.compone
   styleUrl: './deduction-grid.component.css'
 })
 export class DeductionGridComponent {
+  murdleService = inject(MurdleService);
+  separator = '../../../assets/blackline.png'
+  murdleId = '0'
+
+  get rowSuspects() {
+    let suspectsInfo: string[] = [this.separator];
+    const murdleInfo = this.murdleService.getMurdleById(this.murdleId);
+
+
+    const suspects = [
+      murdleInfo!.suspects[0].image,
+      murdleInfo!.suspects[1].image,
+      murdleInfo!.suspects[2].image,
+    ]
+
+    const places = [
+      murdleInfo!.places[0].image,
+      murdleInfo!.places[1].image,
+      murdleInfo!.places[2].image,
+    ]
+
+    suspectsInfo = suspectsInfo.concat(suspects, places);
+
+    return suspectsInfo;
+  }
+
+  get columntSuspects() {
+    const murdleInfo = this.murdleService.getMurdleById(this.murdleId);
+
+    let weapons = [
+      murdleInfo!.weapons[0].image,
+      murdleInfo!.weapons[1].image,
+      murdleInfo!.weapons[2].image,
+    ]
+
+    const places = [
+      murdleInfo!.places[0].image,
+      murdleInfo!.places[1].image,
+      murdleInfo!.places[2].image,
+    ]
+
+    weapons = weapons.concat(places);
+
+    return weapons;
+  }
 
 }
