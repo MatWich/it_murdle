410c72340d18a0acdc1bceb9d247a9265e66aae8
mateuszwicherkski <wicherskimateusz@gmail.com>
2024-09-10 17:13:17 +0200

    suspects as one component

--- src/app/suspect-info/suspect-info.component.ts
+++ src/app/suspect-info/suspect-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { MurdleService } from '../murdle.service';
 import { type Suspect } from './suspect.model';
 
 @Component({
@@ -9,5 +10,46 @@ import { type Suspect } from './suspect.model';
   styleUrl: './suspect-info.component.css'
 })
 export class SuspectInfoComponent {
-  @Input({required: true}) suspects!: Suspect[] ;
-  @Input({required: true}) header!: string;}
+  murdleService = inject(MurdleService);
+  suspectesCategories: string[] = ['Persons', 'Places', 'Weapons']
+  selectedSuspects = '';
+  suspects!: Suspect[] ;
+
+  switchSuspects(suspectsToDisplay: string) {
+    if (suspectsToDisplay === this.selectedSuspects) {
+      this.selectedSuspects = ''
+    } else {
+      this.selectedSuspects = suspectsToDisplay;
+    }
+    this.switchSuspectsInfo(suspectsToDisplay)
+    
+  }
+
+  getWeapons() {
+    this.suspects = this.murdleService.getMurdleById('0')!.weapons;
+  }
+
+  getPersons() {
+    this.suspects = this.murdleService.getMurdleById('0')!.suspects;
+  }
+
+  getPlaces() {
+    this.suspects = this.murdleService.getMurdleById('0')!.places;
+  }
+
+  switchSuspectsInfo(suspectsToDisplay: string) {
+    switch(suspectsToDisplay) {
+      case this.suspectesCategories[0]:
+        this.getPersons()
+        break;
+      case this.suspectesCategories[1]:
+        this.getPlaces()
+        break;
+      case this.suspectesCategories[2]:
+        this.getWeapons()
+        break;
+      default:
+        // nothing to do
+    }
+  }
+}
